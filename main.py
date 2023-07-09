476d0dfae1061df373e942d41397c33daaaccc25
BAVARIA-ONE\Johann <johann.foerster@qualityminds.de>
2023-07-09 16:15:30 +0200

    GLS support

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
-import os, sys, jinja2, csv
+import os, sys, jinja2
+from paypal import Paypal
+from gls import GLS
 from pathlib import Path
-from datetime import datetime
-
-DATE, TIME, NETTO, CURRENCY, DIRECTION = "Datum", "Uhrzeit", "Netto", "Währung", "Auswirkung auf Guthaben"
-NAME, HINT, ARTICLE = "Name", "Hinweis", "Artikelbezeichnung"
-def to_transaction_model(infile):
-    rows, transactions = [], []
-    with open(infile, newline='', mode='r', encoding='utf-8-sig') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            rows.append(row)
-    print(f"Read {len(rows)} transactions from {infile}.")
-    for row in rows:
-        dt = datetime.strptime(f"{row[DATE]} {row[TIME]}", '%d.%m.%Y %H:%M:%S')
-        new_transaction = {'name': row[NAME],
-                           'date': dt.strftime('%Y-%m-%d'),
-                           'description': row[HINT] if row[HINT] else row[ARTICLE],
-                           'amount': (row[NETTO][1:] if row[NETTO].startswith("-") else row[NETTO]).replace(',','.'),
-                           'credit_or_debit': "DBIT" if row[DIRECTION]=="Soll" else "CRDT",
-                           'currency': row[CURRENCY],
-                           'dt': dt
-                          }
-        # skip transactions that are directly paid by credit/debit card
-        is_paypal_transaction = True
-        for idx, transaction in enumerate(transactions):
-            if (transaction['dt'] == new_transaction['dt']) and \
-               (transaction['amount'] == new_transaction['amount']) and \
-               (transaction['credit_or_debit'] == "CRDT" if new_transaction['credit_or_debit']=="DBIT" else "DBIT"):
-                is_paypal_transaction=False
-                transactions.pop(idx)
-                break
-        if is_paypal_transaction:
-            transactions.append(new_transaction)
-    print(f"{len(rows)-len(transactions)} transactions were directly paid by bank transfer.")
-    return {'transactions': transactions}
 
 def main(argv, arc):
     if (arc != 2):
@@ -43,7 +11,12 @@ def main(argv, arc):
         raise Exception(f"{infile} does not exist.")
     if not Path(infile).suffix.lower() == ".csv":
         raise Exception(f"{infile} is not a .csv")
-    model = to_transaction_model(infile)
+
+    model = Paypal(infile).transaction_model()
+    if model == None:
+        model = GLS(infile).transaction_model()
+    if model == None:
+        raise Exception(f"{infile} could not be interpreted as Paypal or GLS")
 
     templateLoader = jinja2.FileSystemLoader(searchpath="./template")
     templateEnv = jinja2.Environment(loader=templateLoader)
@@ -58,4 +31,3 @@ def main(argv, arc):
 
 if __name__ == "__main__":
     main(sys.argv, len(sys.argv))
-
---
+++ paypal.py
@@ -0,0 +1,56 @@
+import csv
+from datetime import datetime
+import inspect
+
+class Paypal:
+    class Fields:
+        DATE, TIME, NETTO, CURRENCY, DIRECTION = "Datum", "Uhrzeit", "Netto", "Währung", "Auswirkung auf Guthaben"
+        NAME, HINT, ARTICLE = "Name", "Hinweis", "Artikelbezeichnung"
+
+    def __init__(self, infile):
+        self.infile, self.rows = infile, []
+        with open(infile, newline='', mode='r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.rows.append(row)
+
+    def is_valid(self):
+        csv_columns = self.rows[0].keys()
+        for key, value in vars(Paypal.Fields).items():
+            if not key.startswith("__"): # skip internal
+                if value not in csv_columns:
+                    return False
+        return True
+
+    def transaction_model(self):
+        if not self.is_valid():
+            return None
+
+        print(f"{self.infile} columns match Paypal .csv style.")
+        print(f"Read {len(self.rows)} Paypal transactions from {self.infile}.")
+
+        rows, transactions = [], []
+        for row in self.rows:
+            dt = datetime.strptime(f"{row[Paypal.Fields.DATE]} {row[Paypal.Fields.TIME]}", '%d.%m.%Y %H:%M:%S')
+            new_transaction = {
+                'name': row[Paypal.Fields.NAME],
+                'date': dt.strftime('%Y-%m-%d'),
+                'description': row[Paypal.Fields.HINT] if row[Paypal.Fields.HINT] else row[Paypal.Fields.ARTICLE],
+                'amount': (row[Paypal.Fields.NETTO][1:] if row[Paypal.Fields.NETTO].startswith("-") else row[Paypal.Fields.NETTO]).replace(',','.'),
+                'credit_or_debit': "DBIT" if row[Paypal.Fields.DIRECTION]=="Soll" else "CRDT",
+                'currency': row[Paypal.Fields.CURRENCY],
+                'dt': dt
+            }
+            # skip transactions that are directly paid by credit/debit card
+            is_paypal_transaction = True
+            for idx, transaction in enumerate(transactions):
+                if (transaction['dt'] == new_transaction['dt']) and \
+                (transaction['amount'] == new_transaction['amount']) and \
+                (transaction['credit_or_debit'] == "CRDT" if new_transaction['credit_or_debit']=="DBIT" else "DBIT"):
+                    is_paypal_transaction=False
+                    transactions.pop(idx)
+                    break
+            if is_paypal_transaction:
+                transactions.append(new_transaction)
+        print(f"{len(self.rows)-len(transactions)} transactions were directly paid by bank transfer.")
+        return {'transactions': transactions}
